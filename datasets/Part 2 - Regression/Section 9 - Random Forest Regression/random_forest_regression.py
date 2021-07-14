a7b8266e1be36ad275cead88938c982f1ba22a81
luis-paloma-smtp <luis.paloma@smtp.health>
2021-07-14 17:46:33 +0200

    random forest uodated

--- datasets/Part 2 - Regression/Section 9 - Random Forest Regression/random_forest_regression.py
+++ datasets/Part 2 - Regression/Section 9 - Random Forest Regression/random_forest_regression.py
@@ -24,7 +24,6 @@ y = dataset.iloc[:, 2].values
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 """
-
 # Escalado de variables
 """from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
